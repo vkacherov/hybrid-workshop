e4b44220859356a81e983fe9ec9be4b1a299da81
Steven Follis <steven_follis@live.com>
2018-04-10 08:13:23 -0400

    Updated template

--- arm_template/install-dtr.sh
+++ arm_template/install-dtr.sh
@@ -1,42 +1,78 @@
-#!/bin/bash
+#!/bin/sh
 #
-# Install Docker Enterprise Edition Engine on Ubuntu
+# Install Docker Trusted Registry on Ubuntu
 
 # UCP URL
-readonly UCP_URL=$1
+readonly UCP_FQDN=$1
 
 # DTR URL
-readonly DTR_URL=$2
+readonly DTR_FQDN=$2
+
+# Node to install DTR on
+readonly UCP_NODE=$(cat /etc/hostname)
 
 # Version of DTR to be installed
 readonly DTR_VERSION="2.5.0-beta3"
 
 # UCP Admin credentials
 readonly UCP_USERNAME="admin"
-readonly UCP_PASSWORD="Docker123!"
+readonly UCP_PASSWORD='Docker123!'
+
+checkDTR() {
+
+    # Check if UCP exists by attempting to hit its load balancer
+    STATUS=$(curl --request GET --url "https://${DTR_FQDN}/_ping" --insecure --silent --output /dev/null -w '%{http_code}' --max-time 5)
+    
+    if [ "$STATUS" -eq 200 ]; then
+        echo "checkDTR: Successfully queried the DTR API. DTR is installed. Joining node to existing cluster."
+        joinDTR
+    else
+        echo "checkDTR: Failed to query the DTR API. DTR is not installed. Installing DTR."
+        installDTR
+    fi
+
+}
 
 installDTR() {
 
-    echo "Installing Docker Trusted Registry (DTR)"
+    echo "installDTR: Installing Docker Trusted Registry (DTR)"
 
     # Install Docker Trusted Registry
-    # Uses port 443 for ease of pulls/pushes
     docker run \
         --rm \
-        docker/dtr:latest install \
-        --dtr-external-url "${DTR_URL}" \
-        --ucp-node dtr01 \
+        docker/dtr:${DTR_VERSION} install \
+        --dtr-external-url "https://${DTR_FQDN}" \
+        --ucp-url "https://${UCP_FQDN}" \
+        --ucp-node "${UCP_NODE}" \
         --ucp-username "${UCP_USERNAME}" \
-        --ucp-password "${UCP_PASSWORD}}" \
-        --ucp-url "${UCP_URL}":8080 \
+        --ucp-password "${UCP_PASSWORD}" \
         --ucp-insecure-tls 
 
-    echo "Finished installing Docker Trusted Registry (DTR)"
+    echo "installDTR: Finished installing Docker Trusted Registry (DTR)"
+
+}
+
+joinDTR() {
+
+    # Get DTR Replica ID
+    REPLICA_ID=$(curl --request GET --insecure --silent --url "https://${DTR_FQDN}/api/v0/meta/settings" -u "${UCP_USERNAME}":"${UCP_PASSWORD}" --header 'Accept: application/json' | jq --raw-output .replicaID)
+    echo "joinDTR: Joining DTR with Replica ID ${REPLICA_ID}"
+
+    # Join an existing Docker Trusted Registry
+    docker run \
+        --rm \
+        docker/dtr:${DTR_VERSION} join \
+        --existing-replica-id "${REPLICA_ID}" \
+        --ucp-url "https://${UCP_FQDN}" \
+        --ucp-node "${UCP_NODE}" \
+        --ucp-username "${UCP_USERNAME}" \
+        --ucp-password "${UCP_PASSWORD}" \
+        --ucp-insecure-tls 
 
 }
 
 main() {
-  installDTR
+  checkDTR
 }
 
 main
--- arm_template/install-ucp.sh
+++ arm_template/install-ucp.sh
@@ -1,68 +1,108 @@
-#!/bin/bash
+#!/bin/sh
 #
-# Install Docker Enterprise Edition Engine on Ubuntu
-
-# Store license URL
-readonly DOCKER_EE_URL=$1
+# Install Docker Universal Control Plane on Ubuntu
 
 # UCP URL
-readonly UCP_URL=$2
+readonly UCP_FQDN=$1
 
-# Repository name for Docker EE Engine
-readonly DOCKER_EE_VERSION="test"
+# External Service Load Balancer URL
+readonly APPS_LB_FQDN=$2
+
+# Is node a worker or manager?
+readonly NODE_ROLE=$3
+
+# Name of current node
+readonly NODE_NAME=$(cat /etc/hostname)
 
 # Version of UCP to be installed
 readonly UCP_VERSION="3.0.0-beta3"
 
-# Version of DTR to be installed
-readonly DTR_VERSION="2.5.0-beta3"
+# UCP Administrator Credentials
+readonly UCP_ADMIN="admin"
+readonly UCP_PASSWORD="Docker123!"
 
-# CIDR of the subnet containing cluster nodes
-readonly AZURE_SUBNET_CIDR="10.0.0.0/24"
+# Install jq library for parsing JSON
+sudo apt-get -qq install jq -y
 
-configure_swarm() {
+checkUCP() {
 
-  # Initiate a Docker Swarm
-  docker swarm init
-
-  # Create secret from toml file
-  docker secret create azure_ucp_admin.toml "./azure_ucp_admin.toml"
-
-  # Use secret in a service to prepopulate VMs with IPs
-  docker service create \
-    --mode=global \
-    --secret=azure_ucp_admin.toml \
-    --log-driver json-file \
-    --log-opt max-size=1m \
-    --name ipallocator \
-    ddebroy/azip
+    # Check if UCP exists by attempting to hit its load balancer
+    STATUS=$(curl --request GET --url "https://${UCP_FQDN}" --insecure --silent --output /dev/null -w '%{http_code}' --max-time 5)
+    
+    if [ "$STATUS" -eq 200 ]; then
+        echo "checkUCP: Successfully queried the UCP API. UCP is installed. Joining node to existing cluster."
+        joinUCP
+    else
+        echo "checkUCP: Failed to query the UCP API. UCP is not installed. Installing UCP."
+        installUCP
+    fi
 
 }
 
 installUCP() {
     
-    echo "Installing Docker Universal Control Plane (UCP)"
+    echo "installUCP: Installing Docker Universal Control Plane (UCP)"
 
     # Install Universal Control Plane
-    # Uses port 8080 to avoid conflict with DTR
     docker run \
         --rm \
         --name ucp \
         --volume /var/run/docker.sock:/var/run/docker.sock \
         docker/ucp:"${UCP_VERSION}" install \
-        --admin-username "admin" \
-        --admin-password "Docker123!" \
-        --san "${UCP_URL}" 
-        #--cloud-provider Azure \
-        #--pod-cidr "${AZURE_SUBNET_CIDR}"
+        --admin-username "${UCP_ADMIN}" \
+        --admin-password "${UCP_PASSWORD}" \
+        --san "${UCP_FQDN}" \
+        --external-service-lb "${APPS_LB_FQDN}"
 
-    echo "Finished installing Docker Universal Control Plane (UCP)"
+    # Wait for node to reach a ready state
+    until [ $(curl --request GET --url "https://${UCP_FQDN}/_ping" --insecure --silent --header 'Accept: application/json' | grep OK) ]
+    do
+        echo '...created cluster, waiting for a ready state'
+        sleep 5
+    done
+
+    echo "installUCP: Cluster's ping returned a ready state"
+
+    echo "installUCP: Finished installing Docker Universal Control Plane (UCP)"
+
+}
+
+joinUCP() {
+
+    # Get Authentication Token
+    AUTH_TOKEN=$(curl --request POST --url "https://${UCP_FQDN}/auth/login" --insecure --silent --header 'Accept: application/json' --data '{ "username": "'${UCP_ADMIN}'", "password": "'${UCP_PASSWORD}'" }' | jq --raw-output .auth_token)
+
+    # Get Swarm Manager IP Address + Port
+    UCP_MANAGER_ADDRESS=$(curl --request GET --url "https://${UCP_FQDN}/info" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq --raw-output .Swarm.RemoteManagers[0].Addr)
+    
+    # Get Swarm Join Tokens
+    UCP_JOIN_TOKENS=$(curl --request GET --url "https://${UCP_FQDN}/swarm" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq .JoinTokens)
+    UCP_JOIN_TOKEN_MANAGER=$(echo "${UCP_JOIN_TOKENS}" | jq --raw-output .Manager)
+    UCP_JOIN_TOKEN_WORKER=$(echo "${UCP_JOIN_TOKENS}" | jq --raw-output .Worker)
+
+    # Join Swarm
+    if [ "$NODE_ROLE" = "Manager" ]
+    then
+        echo "joinUCP: Joining Swarm as a Manager"
+        docker swarm join --token "${UCP_JOIN_TOKEN_MANAGER}" "${UCP_MANAGER_ADDRESS}"
+    else
+        echo "joinUCP: Joining Swarm as a Worker"
+        docker swarm join --token "${UCP_JOIN_TOKEN_WORKER}" "${UCP_MANAGER_ADDRESS}"
+    fi
+
+    # Wait for node to reach a ready state
+    while [ "$(curl --request GET --url "https://${UCP_FQDN}/nodes/${NODE_NAME}" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq --raw-output .Status.State)" != "ready" ]
+    do
+        echo '...node joined, waiting for a ready state'
+        sleep 5
+    done
+
+    echo "joinUCP: Finished joining node to UCP"
 
 }
 
 main() {
-  #configure_swarm
-  installUCP
+  checkUCP
 }
 
 main
